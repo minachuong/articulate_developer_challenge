a59df691b03c806fd7626e2356d8574b90242dbd
Mina Chuong <mina.chuong@gmail.com>
2017-08-12 13:43:13 -0700

    feat: Add WebFontLoader and refactor imported Google Fonts to fonts.css

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'materialize-css/dist/css/materialize.min.css';
 import './index.css';
+import './fonts.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
