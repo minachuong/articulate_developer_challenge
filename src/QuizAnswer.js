56284d640e4925c6c30b3e7d08ee2886178e52b1
Mina Chuong <mina.chuong@gmail.com>
2017-08-14 15:44:36 -0700

    feat: Remove react-inlinesvg

--- src/QuizAnswer.js
+++ src/QuizAnswer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './QuizAnswer.css'
+import Isvg from 'react-inlinesvg'
 
 class QuizAnswer extends Component {  
   render () {
@@ -7,6 +8,7 @@ class QuizAnswer extends Component {
 
     return (
       <div className="quiz-answer">
+        <Isvg src="/icons/icon-check.svg" className="icon"></Isvg>
         <input className="quiz-answer__input" name="answer1" type="radio" id={answer.id} />
         <label htmlFor={answer.id} className="quiz-answer__label">
           <p>{answer.answer}</p>
