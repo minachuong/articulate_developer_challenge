2cbc9fe6fd33a6eedc5ccaa6c4835f2e5b26b356
Mina Chuong <mina.chuong@gmail.com>
2017-08-17 01:15:31 -0700

    feat: Add checking for answer onClick

--- src/QuizCard.js
+++ src/QuizCard.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import './QuizAnswer.css'
 import './QuizCard.css'
 import QuizAnswer from './QuizAnswer'
 
@@ -24,7 +25,13 @@ class QuizCard extends Component {
   }
 
   handleChange = (event) => {
+    this.isCorrect(event.target.value) 
     this.setState({ selectedAnswer: event.target.value })
+  }
+
+  isCorrect = (selectedValue) => {
+    let matchedAnswer = this.state.answers.filter((answer) => answer.id === selectedValue)
+    return matchedAnswer[0].correct
   }
 
   onSubmit = () => {
@@ -37,40 +44,53 @@ class QuizCard extends Component {
 
   addActionsClasses = () => {
     let defaultActionsClasses = "quiz-card__actions "
-    return this.state.submitted ? defaultActionsClasses + "quiz-card__actions--proceed" : defaultActionsClasses
+    return this.state.submitted ? defaultActionsClasses + defaultActionsClasses+"--proceed" : defaultActionsClasses
   }
-
+  
   render() {
     const { question, answers, feedback, imageURL, selectedAnswer } = this.state
 
     return (
       <div className="quiz-card">
-          <div className="quiz-card__title">
-            <p>{question}</p>
-          </div>
-          <figure className="figure-image">
-            <img className="figure-image__image" alt="" src={imageURL} />
-          </figure>
-          <div className="quiz-card__answers">
-          {answers.map((answer) => (
-            <div className="quiz-answer" key={answer.id}>
-               <input type="radio" name="answer1" id={answer.id} value={answer.id} onChange={this.handleChange}/>
-               <label htmlFor={answer.id} className="quiz-answer__label">
-                 <p>{answer.answer}</p>
-               </label>
-             </div>
-          ))}
-          </div>
-          <div className={this.addActionsClasses()}>
-            <div className="quiz-card__submit">
-              <button className={this.addButtonClasses()} type="submit" disabled={selectedAnswer === ""} onClick={this.onSubmit}>Submit</button>
+        <div className="quiz-card__container">
+          <div className="quiz-card__row">
+            <div className="quiz-card__main">
+              <div className="quiz-card__title">
+                <p>{question}</p>
+              </div>
+              <figure className="figure-image">
+                <img className="figure-image__image" alt="" src={imageURL} />
+              </figure>
+              <div className="quiz-card__answers">
+              {answers.map((answer) => (
+                <div className="quiz-answer" key={answer.id}>
+                   <input type="radio" name="answer1" className="quiz-answer__input" id={answer.id} value={answer.id} onClick={this.handleChange}/>
+                   <label htmlFor={answer.id} className="quiz-answer__label">
+                     <span>
+                       <p>{answer.answer}</p>
+                     </span>
+                   </label>
+                 </div>
+              ))}
+              </div>
+              <div className={this.addActionsClasses()}>
+                <div className="quiz-card__submit">
+                  <button className={this.addButtonClasses()} type="submit" disabled={selectedAnswer === ""} onClick={this.onSubmit}>Submit</button>
+                </div>
+              </div>
             </div>
           </div>
-          <div>
-            <div className="card-panel grey lighten-1">
-              <p>{feedback}</p>
+ 
+          <div className="quiz-card__feedback quiz-card__feedback--frame">
+            <div className="quiz-card__row">
+              <div className="quiz-card__main">
+                <div className="quiz-card__feedback-wrap">
+
+                </div>
+              </div>
             </div>
-          </div>
+          </div>  
+        </div>
       </div>  
     )
   }
